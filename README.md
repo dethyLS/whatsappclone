084bfd627c62f3e8851f1c93767ef778baf212ec
dethyLS <74042730+dethyLS@users.noreply.github.com>
2021-03-30 11:42:31 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+To run: Use npm install, and npm start.
+
+The code was made using a youtube tutorial "How To Create A Messaging App With Socket.io And React" from the channel "Web Dev Simplified"
---
+++ build/precache-manifest.08c7da5bb7ebc2cbf7ba9b61e38584b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ebdfc793d42ffbe98652de20855e86e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e090e02d7025ef3ef693",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e090e02d7025ef3ef693",
+    "url": "/static/js/2.acc47649.chunk.js"
+  },
+  {
+    "revision": "bab979f0996c4f83a712fc8810462bad",
+    "url": "/static/js/2.acc47649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b342950ce39736a69e3d",
+    "url": "/static/js/main.a5e64352.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
